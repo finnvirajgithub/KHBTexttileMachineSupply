300cac2082f8c86365e548a192f08920d2f72cff
Sameera Rajapakshe <svirajrajapaksha18@gmail.com>
2025-03-27 13:08:39 +0530

    navbar finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,66 +1,73 @@
-import { Component } from "react";
+import { useState } from "react";
 import { MenuItems } from "./MenuItems";
 import { Link } from "react-router-dom";
 import './NavbarStyles.css'
 import { CgProfile } from "react-icons/cg";
+import { MdMenu } from "react-icons/md";
 import logo from "../assets/logo 3.png"
+import ResponsiveMenu from "./ResponsiveMenu";
 
-class Navbar extends Component {
-    render() {
-        return(
-            <>
-                <nav className="bg-white flex justify-between items-center px-10 shadow-md w-[calc(100%-2rem)] h-20 rounded-xl fixed top-5 left-1/2 -translate-x-1/2 z-50">
-                    <div className="container flex justify-between items-center py-8">
-                        {/* logo section */}
-                        <div className="text-2xl">
-                            <img src={logo} alt="" className="w-55"/>
-                        </div>
+const Navbar = () => {
+    const [open, setOpen] = useState(false);
 
-                        {/* menu section */}
-                        <div className="hidden md:block">
-                            <ul className="flex items-center gap-10 font-bold text-gray-600">
-                                {MenuItems.map((item) => {
-                                    return <li key={item.id}>
-                                        <Link to={item.url} className="inline-block py-1 px-3 hover:text-amber-500 font-semibold">{item.title}</Link></li>;
-                                })}
-                            </ul>
-                        </div>
-                        {/* button section */}
-                        <div className="flex align-middle gap-4">
-                            <button className="text-2xl hover:text-amber-500 rounded-full p-2 duration-200">
-                                <CgProfile />
-                            </button>
-                            <button className="signup hover:bg-amber-500 text-amber-500 font-semibold hover:text-white rounded-md border-2 border-amber-500 duration-200 hidden md:block">
-                                Sign Up
-                            </button>
-                        </div>
+    return(
+        <>
+            <nav className="bg-white flex justify-between items-center px-10 shadow-md w-[calc(100%-2rem)] h-20 rounded-xl fixed top-5 left-1/2 -translate-x-1/2 z-50">
+                <div className="container flex justify-between items-center py-8">
+                    {/* logo section */}
+                    <div className="text-2xl">
+                        <img src={logo} alt="" className="w-55"/>
                     </div>
-                </nav>
-            </>
 
-            // <nav className="bg-white flex justify-between items-center px-10 shadow-md w-[calc(100%-2rem)] h-20 rounded-xl fixed top-5 left-1/2 -translate-x-1/2 z-50 ">
-            //     <h1 className="pl-8">Logo</h1>
-            //     <ul className="flex gap-20">
-            //         {MenuItems.map((item ,index) => {
-            //             return(
-            //                 <li key={index}>
-            //                     <Link to={item.url} className="nav-links no-underline text-gray-900 p-2 hover:bg-amber-600">{item.title}</Link>
-            //                 </li>
-            //             )
-            //         })}
-            //     </ul>
-            //     <div className="flex gap-4">
-            //         <div>
-            //             <i class="fa fa-user" aria-hidden="true"></i>
-            //         </div>
-            //         <div>
-            //             <button>signup</button>
-            //         </div>
-            //     </div>
-                
-            // </nav>
-        )
-    }
-}
+                    {/* menu section */}
+                    <div className="hidden md:block">
+                        <ul className="flex items-center gap-10 font-bold text-gray-600">
+                            {MenuItems.map((item) => {
+                                return <li key={item.id}>
+                                    <Link to={item.url} className="inline-block py-1 px-3 hover:text-amber-500 font-semibold">{item.title}</Link></li>;
+                            })}
+                        </ul>
+                    </div>
+                    {/* button section */}
+                    <div className="flex align-middle gap-4">
+                        <button className="text-2xl hover:text-amber-500 rounded-full p-2 duration-200">
+                            <CgProfile />
+                        </button>
+                        <button className="signup hover:bg-amber-500 text-amber-500 font-semibold hover:text-white rounded-md border-2 border-amber-500 duration-200 hidden md:block">
+                            Sign Up
+                        </button>
+                    </div>
+                    {/* mobile hamberger section */}
+                    <div className="md:hidden" onClick={() => setOpen(!open)}>
+                        <MdMenu className="text-4xl"/>
+                    </div>
+                </div>
+            </nav>
+            <ResponsiveMenu open={open}/>
+        </>
+
+        // <nav className="bg-white flex justify-between items-center px-10 shadow-md w-[calc(100%-2rem)] h-20 rounded-xl fixed top-5 left-1/2 -translate-x-1/2 z-50 ">
+        //     <h1 className="pl-8">Logo</h1>
+        //     <ul className="flex gap-20">
+        //         {MenuItems.map((item ,index) => {
+        //             return(
+        //                 <li key={index}>
+        //                     <Link to={item.url} className="nav-links no-underline text-gray-900 p-2 hover:bg-amber-600">{item.title}</Link>
+        //                 </li>
+        //             )
+        //         })}
+        //     </ul>
+        //     <div className="flex gap-4">
+        //         <div>
+        //             <i class="fa fa-user" aria-hidden="true"></i>
+        //         </div>
+        //         <div>
+        //             <button>signup</button>
+        //         </div>
+        //     </div>
+            
+        // </nav>
+    )
+} 
 
 export default Navbar;
---
+++ src/components/ResponsiveMenu.jsx
@@ -0,0 +1,35 @@
+import { AnimatePresence, motion } from 'motion/react'
+import React from 'react'
+import './NavbarStyles.css'
+import { MenuItems } from './MenuItems'
+import { Link } from "react-router-dom";
+
+const ResponsiveMenu = ({open}) => {
+  return (
+    <AnimatePresence>
+        {
+            open && (
+                <motion.div
+                    initial={{ opacity:0 }}
+                    animate={{ opacity:1 }}
+                    exit={{ opacity:0 }}
+                    className='absolute top-20 left-0 w-full h-screen z-20'
+                >
+                    <div className='mobileMenu text-xl font-semibold uppercase bg-amber-500 text-white py-10 rounded-3xl md:hidden'>
+                        <ul className='flex flex-col justify-center items-center gap-10'>
+                            {MenuItems.map((item) => {
+                                return <li key={item.id}>
+                                    <Link to={item.url}>{item.title}</Link>
+                                </li>
+                            })}
+                        </ul>
+                    </div>
+                </motion.div>
+            )
+        }
+    </AnimatePresence>
+   
+  )
+}
+
+export default ResponsiveMenu
